f1998ccf885c9660d8ada3a5ecf215bf3701eab7
Rudi Sherry <rsherry@adobe.com>
2015-12-23 13:41:43 -0800

    added command-line for local debugging

---
+++ misc/command-line-debug.sh
@@ -0,0 +1,1 @@
+node debug `which lambda-local` -l /Users/rsherry/Documents/Projects/PaiGow/paigow_lambda/lambda-functions/pg-lambda-games-get.js -h handler -e /Users/rsherry/Documents/Projects/PaiGow/paigow_lambda/local-events/pg-lambda-set-tiles-for-deal.json
